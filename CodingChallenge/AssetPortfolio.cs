282a184bd6d379600cb3503c2a94e95d72c5fe72
hosseinn <hosseinn@HOSSEINN-7W2>
2018-10-21 18:53:03 +0200

    Add project files.

---
+++ CodingChallenge/AssetPortfolio.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodingChallenge
+{
+    /// <summary>
+    /// asset portfolio class
+    /// </summary>
+    public class AssetPortfolio
+    {
+        /// <summary>
+        /// Domestic currency 
+        /// </summary>
+        public const string EUR = "EUR";
+
+        public String Name { get; set; }
+
+        public AssetPortfolio(IExchangeRates exchangeRates, string name)
+        {
+            ExchangeRates = exchangeRates;
+            Name = name;
+        }
+
+        public AssetPortfolio(IExchangeRates exchangeRates, string name, List<Asset> assets)
+        {
+            ExchangeRates = exchangeRates;
+            Name = name;
+            assets.ForEach(Add);
+        }
+
+
+        /// <summary>
+        /// list of assets in portfolio
+        /// </summary>
+        public List<Asset> Portfolio { get; } = new List<Asset>();
+
+        private readonly Dictionary<Asset, List<Asset>> _symbols = new Dictionary<Asset, List<Asset>>();
+        public HashSet<Asset> Assets { get;  } = new HashSet<Asset>();
+
+        /// <summary>
+        /// Exchange rates used to value the portfolio
+        /// </summary>
+        public IExchangeRates ExchangeRates { get; set; }
+
+        public void Add(Asset s)
+        {
+            if (_symbols.ContainsKey(s))
+                _symbols[s].Add(s);
+            else
+                _symbols.Add(s, new List<Asset> { s });
+            Portfolio.Add(s);
+            Assets.Add(s); 
+        }
+
+        public decimal Value(string toCurrency)
+        {
+            decimal v = 0;
+            foreach (var s in Portfolio)
+                v = v + s.Value(ExchangeRates, toCurrency);
+            return v;
+        }
+
+        public decimal Value()
+        {
+            return Value(EUR); ;
+        }
+
+        public AssetPortfolio Consolidate()
+        {
+            AssetPortfolio ap = new AssetPortfolio(ExchangeRates, $"consolidated  {Name}");
+            foreach (KeyValuePair<Asset, List<Asset>> asset in _symbols)
+            {
+                Asset newAsset = asset.Key.Consolidate(asset.Value);
+                ap.Portfolio.Add(newAsset);
+            }
+            return ap;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Portfolio '{Name}' value {Value()} EUR with these {Portfolio.Count} assets:");
+            foreach (var asset in Portfolio)
+                sb.AppendLine("\t" + asset);
+            return sb.ToString();
+        }
+    }
+}
